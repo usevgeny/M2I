01ff52e4622f612bb095c1bdc789da4d02147b19
Evgeny <evgeny.rfi@gmail.com>
2022-04-19 10:23:24 +0200

    added unit ant integraiton tests

--- src/test/java/com/drh/api/ApiApplicationTests.java
+++ src/test/java/com/drh/api/ApiApplicationTests.java
@@ -1,15 +1,40 @@
 package com.drh.api;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@SpringBootTest // est importante car on va tester plusieurs classes ensemble , elle va rendre l'applicaiton context disponible 
+@AutoConfigureMockMvc
 class ApiApplicationTests {
-
+	@Autowired
+	MockMvc mockMvc; 
+	
 	@Test
 	void contextLoads() {
 	}
 	
-
+// les test d'integration
+	
+	@Test
+	public void TestGetController() throws Exception{
+		
+		mockMvc.perform(get("/employees")).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$[0].nom", is("Charly")));
+		
+	}
+	
+	
+	
 
 }
---
+++ src/test/java/com/drh/api/ControllerTest.java
@@ -0,0 +1,49 @@
+package com.drh.api;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import org.junit.jupiter.api.Test;
+import org.mockito.plugins.MockMaker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.drh.api.Model.service.EmployeeService;
+import com.drh.api.controller.EmployeeController;
+
+//mockMvc permettent de tester les controlleurs
+//definir un objet mockMVC pour tester les conrolleurs
+@WebMvcTest(controllers = EmployeeController.class)
+public class ControllerTest {
+	
+	@Autowired
+	MockMvc mockMvc;
+	
+	// pour simuler un objet
+	@MockBean
+	private EmployeeService employeeService;
+	
+	@Test
+	public void whenCallingGetMapping_WeReceilve_GoodStatus() throws Exception {
+		
+		mockMvc.perform(get("/employees")).andExpect(status().isOk());}
+		
+	@Test
+	public void whenCallingPostMapping_WeReceilve_GoodStatus() throws Exception {
+			
+		mockMvc.perform(post("/employees")).andExpect(status().isOk());
+			
+		
+	}
+	
+	
+	
+	
+	;
+	
+	
+	
+}
